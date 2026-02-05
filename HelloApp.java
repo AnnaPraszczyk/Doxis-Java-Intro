a63395f484ae39dad8a7fdbb97e8eb96ba9a4341
Anna Praszczyk <74422085+AnnaPraszczyk@users.noreply.github.com>
2026-02-05 13:04:36 +0100

    Add HelloApp class with main method

---
+++ HelloApp.java
@@ -0,0 +1,7 @@
+public class HelloApp {
+    // This is the main entry point for the JVM
+    public static void main(String[] args) {
+        // Printing the message to the console
+        System.out.println("Hello Doxis Team!");
+    }
+}
